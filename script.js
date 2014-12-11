9d15cf5c8f624624da0f7ca14382c05ff3c7ae49
Josh Lee <Lee45276@gmail.com>
2014-12-11 15:21:42 -0600

    Added more people

--- script.js
+++ script.js
@@ -10,12 +10,13 @@ $('button').click(function(){
         
         var i = 1;
         
-        while( i < 7;) {
+        while( i < 7) {
             alert(userSalary);
-            var salary1 = $('#salary1').html(); // gets value from richie rich
+            var salary1 = $('#salary' + i).html(); // gets value from richie rich
             var salary1Format = parseFloat(salary1.replace(/\$|,/g, '')); // removes characters so can compute
             var years1Calc = salary1Format / userSalary;
-            $('#years1').html(Math.round(years1Calc) + " years");
+            $('#years' + i).html(Math.ceil(years1Calc) + " years");
+            i += 1;
         }
         
         
