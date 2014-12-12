03c5203d326d9cfca0d32ac7bc9cf2f9afe5557b
Josh Lee <Lee45276@gmail.com>
2014-12-11 20:15:03 -0600

    Added about section, finished fade ins and styling

--- script.js
+++ script.js
@@ -1,28 +1,48 @@
+var j = 1;
+var k = 1;
 
 
-$('button').click(function(){
+$('#start').click(function(){
    var userSalary = $('#userInput').val();
     if (!($.isNumeric(userSalary))){
         alert("please enter a number");
     } else {
+         $('.person_div').each(function() {
+        $(this).fadeOut();
+    });
         userSalary = Math.round(userSalary);
-        
-        
         var i = 1;
-        
-        while( i < 7) {
-            alert(userSalary);
+        while( i < 22) {
+            var currentDiv = "#person_" + i;
             var salary1 = $('#salary' + i).html(); // gets value from richie rich
             var salary1Format = parseFloat(salary1.replace(/\$|,/g, '')); // removes characters so can compute
             var years1Calc = salary1Format / userSalary;
-            $('#years' + i).html(Math.ceil(years1Calc) + " years");
-            i += 1;
-        }
+            
+            if (Math.ceil(years1Calc) < 2){
+            $('#years' + i).html(Math.ceil(years1Calc) + " year");
+            } else {
+                $('#years' + i).html(Math.ceil(years1Calc) + " years");
+            }
+            i += 1;   
+                      }
         
-        
-        
-        
-        
-    }
+            $('.person_div').each(function(j) {
+            $(this).delay(300*(j+1)).fadeIn(500);
+            });
+          }
+ 
+                        }
+                 );
+
+
+
+$('#close').click(function() {
+    $('.popup').fadeOut();
 });
+
+$('#info').click(function() {
+    $('.popup').fadeIn();
+});
+
+
 
